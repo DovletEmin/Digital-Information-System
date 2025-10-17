cf9c7e2883d430b835032940c0b753c749fca665
Dowlet Eminow <dovlet.eminov26.02@gmail.com>
2025-10-17 09:25:30 +0500

    Added Article model

--- src/content/models.py
+++ src/content/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Article(models.Model):
+    LANGUAGE_CHOICES = [
+        ('tm', 'Turkmen'),
+        ('ru', 'Russian'),
+        ('en', 'English'),
+    ]
+
+    TYPE_CHOICES = [
+        ('local', 'Local'),
+        ('foreign', 'Foreign'),
+    ]
+
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    author_workplace = models.CharField(max_length=255, blank=True, null=True)
+    rating = models.FloatField(default=0.0)
+    views = models.IntegerField(default=0)
+    bookmarks = models.ManyToManyField(User, related_name='bookmarked_articles', blank=True) 
+    language = models.CharField(max_length=2, choices=LANGUAGE_CHOICES, default='tm')
+    type = models.CharField(max_length=7, choices=TYPE_CHOICES, default='local')
+    publication_date = models.DateField()
+    source_name = models.CharField(max_length=255, blank=True, null=True)  
+    source_url = models.URLField(blank=True, null=True) 
+    newspaper_or_journal = models.CharField(max_length=255, blank=True, null=True)  
+    
+    def __str__(self):
+        return self.title
