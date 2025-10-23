d1e8935413f840bef52b27294b53c4053732c32f
Dowlet Eminow <dovlet.eminov26.02@gmail.com>
2025-10-23 12:46:21 +0500

    Some fixes of views.py file

--- src/content/views.py
+++ src/content/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import (
     Article, Book, Dissertation,
     ArticleCategory, BookCategory, DissertationCategory
@@ -8,37 +9,51 @@ from .serializers import (
     ArticleCategorySerializer, BookCategorySerializer, DissertationCategorySerializer
 )
 
-
 class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-    filterset_fields = ['language', 'type', 'categories']
-
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = {
+        'language': ['exact'],
+        'type': ['exact'],
+        'categories': ['exact'],
+        'publication_date': ['gte', 'lte', 'exact'],  # >=, <=, =
+    }
 
 class BookViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    filterset_fields = ['language', 'categories']
-
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = {
+        'language': ['exact'],
+        'categories': ['exact'],
+    }
 
 class DissertationViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Dissertation.objects.all()
     serializer_class = DissertationSerializer
-    filterset_fields = ['language', 'categories']
-
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = {
+        'language': ['exact'],
+        'categories': ['exact'],
+    }
 
 class ArticleCategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = ArticleCategory.objects.all()
     serializer_class = ArticleCategorySerializer
 
-
 class BookCategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = BookCategory.objects.all()
     serializer_class = BookCategorySerializer
-    filterset_fields = ['parent']
-
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = {
+        'parent': ['exact', 'isnull'],  # parent=ID или parent__isnull=true
+    }
 
 class DissertationCategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = DissertationCategory.objects.all()
     serializer_class = DissertationCategorySerializer
-    filterset_fields = ['parent']
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = {
+        'parent': ['exact', 'isnull'],
+    }
