f612a659bb541a892bf7f1f350920bc205ddb6cc
Dowlet Eminow <dovlet.eminov26.02@gmail.com>
2025-10-23 11:27:11 +0500

    Added BookSerializer

--- src/content/serializers.py
+++ src/content/serializers.py
@@ -11,8 +11,8 @@ class ArticleCategorySerializer(serializers.ModelSerializer):
         fields = ['id', 'name']
 
 
-class BookCategorySeralizer(serializers.ModelSerializer):
-    parent = serializers.PrimaryKeyRelatedField(queryset=BookCategory.objects.all(), allow_null=True)
+class BookCategorySerializer(serializers.ModelSerializer):
+    parent = serializers.PrimaryKeyRelatedField(read_only=True)
     subcategories = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
@@ -21,7 +21,7 @@ class BookCategorySeralizer(serializers.ModelSerializer):
 
 
 class DissertationCategorySerializer(serializers.ModelSerializer):
-    parent = serializers.PrimaryKeyRelatedField(queryset=DissertationCategory.objects.all(), allow_null=True)
+    parent = serializers.PrimaryKeyRelatedField(read_only=True)
     subcategories = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
@@ -38,3 +38,17 @@ class ArticleSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'content', 'author', 'author_workplace', 'rating', 'views',
                   'language', 'type', 'publication_date', 'source_name', 'source_url',
                   'newspaper_or_journal', 'categories', 'bookmarks']
+        read_only_fields = fields
+        
+
+class BookSerializer(serializers.ModelField):
+    categories = BookCategorySerializer(many=True, read_only=True)
+    bookmarks = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    cover_image = serializers.ImageField(read_only=True)
+    epub_file = serializers.FileField(read_only=True)
+
+    class Meta:
+        model = Book
+        fields = ['id', 'title', 'content', 'epub_file', 'cover_image', 'author',
+                  'rating', 'views', 'language', 'categories', 'bookmarks']
+        read_only_fields = fields
