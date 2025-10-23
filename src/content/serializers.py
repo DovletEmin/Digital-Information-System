2937e1ca2bd7527b4573a10bd19daf8e529ec9ae
Dowlet Eminow <dovlet.eminov26.02@gmail.com>
2025-10-23 11:17:52 +0500

    Added ArticleSerializer

--- src/content/serializers.py
+++ src/content/serializers.py
@@ -27,3 +27,14 @@ class DissertationCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = DissertationCategory
         fields = ['id', 'name', 'parent', 'subcategories']
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    categories = ArticleCategorySerializer(many=True, read_only=True)
+    bookmarks = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Article
+        fields = ['id', 'title', 'content', 'author', 'author_workplace', 'rating', 'views',
+                  'language', 'type', 'publication_date', 'source_name', 'source_url',
+                  'newspaper_or_journal', 'categories', 'bookmarks']
