74dfa1542e07db8d4e71a4596c4c259935a5eb66
Dowlet Eminow <dovlet.eminov26.02@gmail.com>
2025-10-23 09:31:46 +0500

    Added Book_Category

--- src/content/models.py
+++ src/content/models.py
@@ -7,3 +7,16 @@ class ArticleCategory(models.Model):
     def __str__(self):
         return self.name
     
+
+class BookCategory(models.Model):
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name="subcategories")
+
+    class Meta:
+        unique_together = ('name', 'parent')
+
+    def __str__(self):
+        if self.parent:
+            return f"{self.parent.name} > {self.name}"
+        return self.name
+    
