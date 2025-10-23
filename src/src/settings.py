93f2d1e8452edaf5b7032bff374daeeea2f8bbd7
Dowlet Eminow <dovlet.eminov26.02@gmail.com>
2025-10-23 10:37:57 +0500

    Some fixes in settings file

--- src/src/settings.py
+++ src/src/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7n_(2oo+g=p!sqgyy^^-qb-l99xsg09^2@ikglgbh4kp4ovdwo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
