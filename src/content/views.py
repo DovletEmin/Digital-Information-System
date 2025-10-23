cdfcad8586f16cb59e431d18d7f2413d7dad95f0
Dowlet Eminow <dovlet.eminov26.02@gmail.com>
2025-10-23 11:41:12 +0500

    Added ArticleViewSet

--- src/content/views.py
+++ src/content/views.py
@@ -7,3 +7,10 @@ from .serializers import (
     ArticleSerializer, BookSerializer, DissertationSerializer,
     ArticleCategorySerializer, BookCategorySerializer, DissertationCategorySerializer
 )
+
+
+class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    filterset_fields = ['language', 'type', 'categories']
+    
