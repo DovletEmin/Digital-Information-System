91d6ef5a68b4b189153f0efdf7a47f5ee28b1595
Dowlet Eminow <dovlet.eminov26.02@gmail.com>
2025-10-23 10:13:51 +0500

    Connected models with admin.py

--- src/content/admin.py
+++ src/content/admin.py
@@ -1,1 +1,12 @@
 from django.contrib import admin
+from .models import (
+    Article, Book, Dissertation,
+    ArticleCategory, BookCategory, DissertationCategory
+)
+
+admin.site.register(Article)
+admin.site.register(Book)
+admin.site.register(Dissertation)
+admin.site.register(ArticleCategory)
+admin.site.register(BookCategory)
+admin.site.register(DissertationCategory)
