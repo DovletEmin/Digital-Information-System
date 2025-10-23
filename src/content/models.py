34af245df2ece61e08d8191efc82b7afc6220e03
Dowlet Eminow <dovlet.eminov26.02@gmail.com>
2025-10-23 09:35:16 +0500

    Added DissertationCategory

--- src/content/models.py
+++ src/content/models.py
@@ -20,3 +20,16 @@ class BookCategory(models.Model):
             return f"{self.parent.name} > {self.name}"
         return self.name
     
+
+class DissertationCategory(models.Model):
+    name = models.CharField(max_length = 100)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='subcategories')
+
+    class Meta:
+        unique_together = ('name', 'parent')
+    
+    def __str__(self):
+        if self.parent:
+            return f"{self.parent.name} > {self.name}"
+        return self.name
+    
