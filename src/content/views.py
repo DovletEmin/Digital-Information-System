41190b7fd2fba19c18144d84fe35676dd212d151
Dowlet Eminow <dovlet.eminov26.02@gmail.com>
2025-10-23 11:38:49 +0500

    Created skeleton of views.py file

--- src/content/views.py
+++ src/content/views.py
@@ -1,1 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import (
+    Article, Book, Dissertation,
+    ArticleCategory, BookCategory, DissertationCategory
+)
+from .serializers import (
+    ArticleSerializer, BookSerializer, DissertationSerializer,
+    ArticleCategorySerializer, BookCategorySerializer, DissertationCategorySerializer
+)
