b1758ac25a6011a78079c40486a1e54de49bd58e
Dowlet Eminow <dovlet.eminov26.02@gmail.com>
2025-10-23 10:59:01 +0500

    Added Rest_Framework settings in settings.py

--- src/src/settings.py
+++ src/src/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'content',
+    'rest_framework',
+    'def_yasg',
 ]
 
 MIDDLEWARE = [
@@ -70,6 +72,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'src.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 25,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',  
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
