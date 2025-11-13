e1e057b00451da6ea31b1179c06eb82ea95cb625
Dowlet Eminow <dovlet.eminov26.02@gmail.com>
2025-11-13 10:18:34 +0500

    Updated page_size in settings.py

--- src/src/settings.py
+++ src/src/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 25,
+    'PAGE_SIZE': 3,
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
