b8d6b35ca35c4f4bbc318f45317acac181167f10
Dowlet Eminow <dovlet.eminov26.02@gmail.com>
2025-11-21 12:07:07 +0500

    Added API endpoints for auth

--- src/content/serializers.py
+++ src/content/serializers.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import (
     Article, Book, Dissertation,
     ArticleCategory, BookCategory, DissertationCategory
 )
 
+
 class ArticleCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = ArticleCategory
         fields = ['id', 'name']
+
 
 class BookCategorySerializer(serializers.ModelSerializer):
     parent = serializers.PrimaryKeyRelatedField(read_only=True)
@@ -18,7 +21,8 @@ class BookCategorySerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'parent', 'subcategories']
 
     def get_subcategories(self, obj):
-        return [category.id for category in obj.subcategories.all()]
+        return [cat.id for cat in obj.subcategories.all()]
+
 
 class DissertationCategorySerializer(serializers.ModelSerializer):
     parent = serializers.PrimaryKeyRelatedField(read_only=True)
@@ -29,46 +33,80 @@ class DissertationCategorySerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'parent', 'subcategories']
 
     def get_subcategories(self, obj):
-        return [category.id for category in obj.subcategories.all()]
+        return [cat.id for cat in obj.subcategories.all()]
+
 
 class ArticleSerializer(serializers.ModelSerializer):
     categories = ArticleCategorySerializer(many=True, read_only=True)
-    bookmarks = serializers.SerializerMethodField()
+    is_bookmarked = serializers.SerializerMethodField()
 
     class Meta:
         model = Article
-        fields = ['id', 'title', 'content', 'author', 'author_workplace', 'rating', 'views',
-                  'language', 'type', 'publication_date', 'source_name', 'source_url',
-                  'newspaper_or_journal', 'categories', 'bookmarks', 'image']
+        fields = [
+            'id', 'title', 'content', 'author', 'author_workplace',
+            'rating', 'views', 'language', 'type', 'publication_date',
+            'source_name', 'source_url', 'newspaper_or_journal',
+            'categories', 'image', 'is_bookmarked'
+        ]
         read_only_fields = fields
 
-    def get_bookmarks(self, obj):
-        return [user.id for user in obj.bookmarks.all()]
+    def get_is_bookmarked(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return user.profile.bookmarks_articles.filter(pk=obj.id).exists()
+        return False
+
 
 class BookSerializer(serializers.ModelSerializer):
     categories = BookCategorySerializer(many=True, read_only=True)
-    bookmarks = serializers.SerializerMethodField()
-    cover_image = serializers.ImageField(read_only=True)
-    epub_file = serializers.FileField(read_only=True)
+    is_bookmarked = serializers.SerializerMethodField()
 
     class Meta:
         model = Book
-        fields = ['id', 'title', 'content', 'epub_file', 'cover_image', 'author',
-                  'rating', 'views', 'language', 'categories', 'bookmarks']
+        fields = [
+            'id', 'title', 'content', 'epub_file', 'cover_image',
+            'author', 'rating', 'views', 'language',
+            'categories', 'is_bookmarked'
+        ]
         read_only_fields = fields
 
-    def get_bookmarks(self, obj):
-        return [user.id for user in obj.bookmarks.all()]
+    def get_is_bookmarked(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return user.profile.bookmarks_books.filter(pk=obj.id).exists()
+        return False
+
 
 class DissertationSerializer(serializers.ModelSerializer):
     categories = DissertationCategorySerializer(many=True, read_only=True)
-    bookmarks = serializers.SerializerMethodField()
+    is_bookmarked = serializers.SerializerMethodField()
 
     class Meta:
         model = Dissertation
-        fields = ['id', 'title', 'content', 'author', 'author_workplace', 'rating',
-                  'views', 'language', 'publication_date', 'categories', 'bookmarks']
+        fields = [
+            'id', 'title', 'content', 'author', 'author_workplace',
+            'rating', 'views', 'language', 'publication_date',
+            'categories', 'is_bookmarked'
+        ]
         read_only_fields = fields
 
-    def get_bookmarks(self, obj):
-        return [user.id for user in obj.bookmarks.all()]
+    def get_is_bookmarked(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return user.profile.bookmarks_dissertations.filter(pk=obj.id).exists()
+        return False
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
+        )
+        return user
