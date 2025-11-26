be6b6e7494dcbd0fd5e7e1e4b37ef01c50631005
Dowlet Eminow <dovlet.eminov26.02@gmail.com>
2025-11-26 11:55:02 +0500

    Added tests for elasticsearch endpoints

--- src/content/tests.py
+++ src/content/tests.py
@@ -38,13 +38,13 @@ class APITest(APITestCase):
             views=100,
             language="tm",
             type="local",
-            publication_date=date(2023, 1, 1),
+            publication_date=date(2023, 1, 15),
             source_name="Test Source",
             source_url="http://example.com",
             newspaper_or_journal="Test Journal",
         )
         self.article.categories.add(self.article_category)
-        self.article.bookmarks.add(self.user)
+        self.user.profile.bookmarks_articles.add(self.article)
 
         self.book = Book.objects.create(
             title="Test Book",
@@ -57,7 +57,7 @@ class APITest(APITestCase):
             language="ru",
         )
         self.book.categories.add(self.book_subcategory)
-        self.book.bookmarks.add(self.user)
+        self.user.profile.bookmarks_books.add(self.book)
 
         self.dissertation = Dissertation.objects.create(
             title="Test Dissertation",
@@ -70,18 +70,16 @@ class APITest(APITestCase):
             publication_date=date(2024, 6, 15),
         )
         self.dissertation.categories.add(self.dissertation_subcategory)
-        self.dissertation.bookmarks.add(self.user)
+        self.user.profile.bookmarks_dissertations.add(self.dissertation)
 
+    # --- Article tests ---
     def test_get_article_list(self):
         url = reverse("article-list")
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data["results"]), 1)
         self.assertEqual(response.data["results"][0]["title"], "Test Article")
-        self.assertEqual(
-            response.data["results"][0]["categories"][0]["name"], "Science"
-        )
-        self.assertEqual(response.data["results"][0]["bookmarks"], [self.user.id])
+        self.assertEqual(response.data["results"][0]["categories"][0]["name"], "Science")
 
     def test_get_article_detail(self):
         url = reverse("article-detail", kwargs={"pk": self.article.id})
@@ -109,39 +107,30 @@ class APITest(APITestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data["results"]), 1)
-        self.assertEqual(
-            response.data["results"][0]["categories"][0]["name"], "Science"
-        )
+        self.assertEqual(response.data["results"][0]["categories"][0]["name"], "Science")
 
     def test_article_filter_by_date_range(self):
-        url = (
-            reverse("article-list")
-            + "?publication_date__gte=2023-01-01&publication_date__lte=2023-12-31"
-        )
+        url = reverse("article-list") + "?publication_date__gte=2023-01-01&publication_date__lte=2023-12-31"
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data["results"]), 1)
-        self.assertEqual(response.data["results"][0]["publication_date"], "2023-01-01")
+        self.assertEqual(response.data["results"][0]["publication_date"], "15.01.2023")
 
     def test_article_combined_filter(self):
-        url = (
-            reverse("article-list")
-            + f"?language=tm&type=local&categories={self.article_category.id}&publication_date__gte=2023-01-01"
-        )
+        url = reverse("article-list") + f"?language=tm&type=local&categories={self.article_category.id}&publication_date__gte=2023-01-01"
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data["results"]), 1)
         self.assertEqual(response.data["results"][0]["title"], "Test Article")
 
+    # --- Book tests ---
     def test_get_book_list(self):
         url = reverse("book-list")
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data["results"]), 1)
         self.assertEqual(response.data["results"][0]["title"], "Test Book")
-        self.assertEqual(
-            response.data["results"][0]["categories"][0]["name"], "Biology"
-        )
+        self.assertEqual(response.data["results"][0]["categories"][0]["name"], "Biology")
 
     def test_get_book_detail(self):
         url = reverse("book-detail", kwargs={"pk": self.book.id})
@@ -162,9 +151,7 @@ class APITest(APITestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data["results"]), 1)
-        self.assertEqual(
-            response.data["results"][0]["categories"][0]["name"], "Biology"
-        )
+        self.assertEqual(response.data["results"][0]["categories"][0]["name"], "Biology")
 
     def test_get_dissertation_list(self):
         url = reverse("dissertation-list")
@@ -172,9 +159,7 @@ class APITest(APITestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data["results"]), 1)
         self.assertEqual(response.data["results"][0]["title"], "Test Dissertation")
-        self.assertEqual(
-            response.data["results"][0]["categories"][0]["name"], "Physics"
-        )
+        self.assertEqual(response.data["results"][0]["categories"][0]["name"], "Physics")
 
     def test_get_dissertation_detail(self):
         url = reverse("dissertation-detail", kwargs={"pk": self.dissertation.id})
@@ -191,65 +176,29 @@ class APITest(APITestCase):
         self.assertEqual(response.data["results"][0]["language"], "en")
 
     def test_dissertation_filter_by_category(self):
-        url = (
-            reverse("dissertation-list")
-            + f"?categories={self.dissertation_subcategory.id}"
-        )
+        url = reverse("dissertation-list") + f"?categories={self.dissertation_subcategory.id}"
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data["results"]), 1)
-        self.assertEqual(
-            response.data["results"][0]["categories"][0]["name"], "Physics"
-        )
+        self.assertEqual(response.data["results"][0]["categories"][0]["name"], "Physics")
 
     def test_get_article_category_list(self):
         url = reverse("articlecategory-list")
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data["results"]), 1)
-        self.assertEqual(response.data["results"][0]["name"], "Science")
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["name"], "Science")
 
     def test_get_book_category_list(self):
         url = reverse("bookcategory-list")
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data["results"]), 2)
-        self.assertEqual(response.data["results"][0]["name"], "Scientific")
+        self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data[0]["name"], "Scientific")
 
     def test_book_category_filter_by_parent(self):
         url = reverse("bookcategory-list") + f"?parent={self.book_top_category.id}"
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data["results"]), 1)
-        self.assertEqual(response.data["results"][0]["name"], "Biology")
-
-    def test_book_category_filter_by_top_level(self):
-        url = reverse("bookcategory-list") + "?parent__isnull=true"
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data["results"]), 1)
-        self.assertEqual(response.data["results"][0]["name"], "Scientific")
-
-    def test_get_dissertation_category_list(self):
-        url = reverse("dissertationcategory-list")
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data["results"]), 2)
-        self.assertEqual(response.data["results"][0]["name"], "Academic")
-
-    def test_dissertation_category_filter_by_parent(self):
-        url = (
-            reverse("dissertationcategory-list")
-            + f"?parent={self.dissertation_top_category.id}"
-        )
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data["results"]), 1)
-        self.assertEqual(response.data["results"][0]["name"], "Physics")
-
-    def test_dissertation_category_filter_by_top_level(self):
-        url = reverse("dissertationcategory-list") + "?parent__isnull=true"
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data["results"]), 1)
-        self.assertEqual(response.data["results"][0]["name"], "Academic")
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["name"], "Biology")
